1876e331fd22be91fc6047af735b2c332083e42d
raminafee <raminafe2002@hotmail.com>
2025-07-14 12:30:30 +0300

    Create script2.js

---
+++ static/script2.js
@@ -0,0 +1,27 @@
+document.getElementById("data-form").addEventListener("submit", function (event) {
+    event.preventDefault();  
+    var formData = new FormData();
+    formData.append("file", document.getElementById("file-input").files[0]); 
+
+    fetch('/analyze2', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(errorData => { throw new Error(errorData.error || 'خطأ غير معروف'); });
+            }
+            return response.json();
+        })
+        .then(data => {
+           
+            document.getElementById("text-analysis").innerText = data.textAnalysis;
+
+            
+            document.getElementById("data-chart").src = 'data:image/png;base64,' + data.imageBase64;
+        })
+        .catch(error => {
+            console.error("Error analyzing data:", error);
+            alert("حدث خطأ: " + error.message); 
+        });
+});
